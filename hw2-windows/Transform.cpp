6b20a6411c54a5876f04c58fbf728f06930b07f2
Elliot Morales Solé <elliotmoso@gmail.com>
2013-10-16 01:12:46 +0200

    Added Rotation function

--- hw2-windows/Transform.cpp
+++ hw2-windows/Transform.cpp
@@ -9,10 +9,19 @@
 // Helper rotation function.  Please implement this.  
 mat3 Transform::rotate(const float degrees, const vec3& axis) 
 {
-    mat3 ret;
-    // YOUR CODE FOR HW2 HERE
-    // Please implement this.  Likely the same as in HW 1.  
-    return ret;
+   float radians = degrees * pi / 180.0f;
+    mat3 r1(cos(radians));
+    mat3 r2(0, -axis.z, axis.y, axis.z, 0, -axis.x, -axis.y, axis.x, 0);
+    mat3 r3(axis.x*axis.x, axis.x*axis.y, axis.x*axis.z,
+            axis.x*axis.y, axis.y*axis.y, axis.y*axis.z,
+            axis.x*axis.z, axis.z*axis.y, axis.z*axis.z);
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            r2[i][j] = r2[i][j]*sin(radians);
+            r3[i][j] = r3[i][j]*(1-cos(radians));
+        }
+    }
+    return r1 + r2 + r3;
 }
 
 void Transform::left(float degrees, vec3& eye, vec3& up) 
